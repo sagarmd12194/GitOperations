6c6def59731fc1158afa066b618510f075fbe64f
sagarmd12194 <sagarmd888@gmail.com>
2022-01-26 09:33:12 +0530

    Java classes created

---
+++ Git_java_prj/src/my/sagar/pack/Class2.java
@@ -0,0 +1,8 @@
+package my.sagar.pack;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is class 2");
+	}
+}
---
+++ Git_java_prj/src/my/sagar/pack/Class3.java
@@ -0,0 +1,8 @@
+package my.sagar.pack;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		System.out.println("This is class 3");
+	}
+}
