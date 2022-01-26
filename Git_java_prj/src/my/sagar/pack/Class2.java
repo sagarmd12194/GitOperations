1978dfc5a9f8b3b3912ba2b9ee6140c92a8bdba1
sagarmd12194 <sagarmd888@gmail.com>
2022-01-26 09:59:58 +0530

    added new class

--- Git_java_prj/src/my/sagar/pack/Class2.java
+++ Git_java_prj/src/my/sagar/pack/Class2.java
@@ -4,6 +4,6 @@ public class Class2 {
 
 	public static void main(String[] args) {
 		System.out.println("This is class, updated with new changes");
-		System.out.println("new print");
+		System.out.println("new print added in branch_2");
 	}
 }
---
+++ Git_java_prj/src/my/sagar/pack/Class5.java
@@ -0,0 +1,9 @@
+package my.sagar.pack;
+
+public class Class5 {
+
+	public static void main(String[] args) {
+		System.out.println("This is class 5");
+		System.out.println("new print now in new class 5");
+	}
+}
