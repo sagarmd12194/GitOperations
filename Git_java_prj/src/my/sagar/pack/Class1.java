33e6b9f9aaff5bcd28f049457eade5c5be616711
sagarmd12194 <sagarmd888@gmail.com>
2022-01-26 09:49:08 +0530

    changed

--- Git_java_prj/src/my/sagar/pack/Class1.java
+++ Git_java_prj/src/my/sagar/pack/Class1.java
@@ -3,6 +3,6 @@ package my.sagar.pack;
 public class Class1 {
 
 	public static void main(String[] args) {
-		System.out.println("This is class 1.0, update");
+		System.out.println("This is class new changes added");
 	}
 }
