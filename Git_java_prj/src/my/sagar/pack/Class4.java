592445040a3fa49696318ddde094487ae4c303a3
sagarmd12194 <sagarmd888@gmail.com>
2022-01-26 09:48:11 +0530

    changed

--- Git_java_prj/src/my/sagar/pack/Class4.java
+++ Git_java_prj/src/my/sagar/pack/Class4.java
@@ -1,9 +1,9 @@
 package my.sagar.pack;
 
-public class Class3 {
+public class Class4 {
 
 	public static void main(String[] args) {
-		System.out.println("This is class 3");
+		System.out.println("This is class 4");
 		System.out.println("new print");
 	}
 }
