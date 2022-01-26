3318fd4b435346942c143de6deed152b0456b684
sagar <36165801+sagarmd12194@users.noreply.github.com>
2022-01-26 09:36:13 +0530

    Update Class2.java

--- Git_java_prj/src/my/sagar/pack/Class2.java
+++ Git_java_prj/src/my/sagar/pack/Class2.java
@@ -3,6 +3,6 @@ package my.sagar.pack;
 public class Class2 {
 
 	public static void main(String[] args) {
-		System.out.println("This is class 2");
+		System.out.println("This is class 2.0, update");
 	}
 }
